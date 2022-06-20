5063f48f139a15b4ca9adba554f5c27161ba0f29
Wessel Kuijstermans <w.kuijstermans@student.avans.nl>
2022-06-20 16:22:45 +0200

    Added permission popup and camera switching

--- app/src/main/java/com/example/hetrondjeandroid/MainActivity.java
+++ app/src/main/java/com/example/hetrondjeandroid/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.hetrondjeandroid;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -11,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,6 +23,7 @@ import com.example.hetrondjeandroid.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,13 +32,26 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private Camera mCamera;
     private CameraPreview mPreview;
+    private Button mSwitchCamera;
+    private int currentCamId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_activity);
 
+        checkPermission();
+
+
         // Create an instance of Camera
         mCamera = getCameraInstance();
+        currentCamId =  0;
+        mSwitchCamera = findViewById(R.id.button_switch);
+        mSwitchCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchCamera();
+            }
+        });
 
         // Create our Preview view and set it as the content of our activity.
         mPreview = new CameraPreview(this, mCamera);
@@ -48,6 +65,37 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    public void switchCamera() {
+        mCamera.release();
+        if (currentCamId == 0) {
+            currentCamId = 1;
+        } else {
+            currentCamId = 0;
+        }
+        mCamera = Camera.open(currentCamId);
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+
+
+
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.RECORD_AUDIO,}, 1);
+            }
+        }
+    }
+
+
 
 
 
